38b7d3fe4b0be708724f0b524e13e6ab219c6bd2
donghym@yonyou.com <donghym@yonyou.com>
2018-08-10 14:39:09 +0800

    笛卡尔积 进入全屏 退出全屏

--- src/index.js
+++ src/index.js
@@ -246,7 +246,51 @@ Number.prototype.toFixed = function(n) {
     }
     return result;
 };
-
+// 生成笛卡尔积数组
+const combine = (arr) => {
+    let r = [];
+    (function f(t, a, n) {
+        if (n == 0) return r.push(t);
+        for (var i = 0; i < a[n - 1].length; i++) {
+            f(t.concat(a[n - 1][i]), a, n - 1);
+        }
+    })([], arr, arr.length);
+    r = r.map(v => {
+        let obj = {}
+        v.map(val => {
+            obj = Object.assign(obj, val)
+        })
+        return obj
+    })
+    return r;
+}
+// 进入全屏
+const requestFullScreen = () => {
+    let elem = document.documentElement
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen()
+    } else if (elem.mozRequestFullScreen) {
+        elem.mozRequestFullScreen()
+    } else if (elem.webkitRequestFullScreen) {
+        elem.webkitRequestFullScreen()
+    } else if (elem.msRequestFullscreen) {
+        // elem.msRequestFullscreen() 没有指定元素
+        document.body.msRequestFullscreen()
+    }
+}
+// 退出全屏
+const exitFullscreen = () => {
+    let doc = document
+    if (doc.exitFullscreen) {
+        doc.exitFullscreen()
+    } else if (doc.mozCancelFullScreen) {
+        doc.mozCancelFullScreen()
+    } else if (doc.webkitCancelFullScreen) {
+        doc.webkitCancelFullScreen()
+    } else if (doc.msExitFullscreen) {
+        doc.msExitFullscreen()
+    }
+}
 module.exports = {
     typeOf: typeOf,
     isArray: isArray,
@@ -254,11 +298,12 @@ module.exports = {
     isNumber: isNumber,
     isObject: isObject,
     isBoolean: isBoolean,
-    isString: isString,
-    isUndefined: isUndefined,
-    isNull: isNull,
-    isNil: isNil,
-    isEmpty: isEmpty,
+    isString: isString, 
+    isUndefined: isUndefined, // 判断是不是undefined
+    isNull: isNull, // 判断是不是null
+    isNil: isNil, // 判断是不是 null或undefined
+    isEmpty: isEmpty, 
+    isObjectNull: isObjectNull, // 判断对象是否为空
     isEmail: isEmail,
     chechCHNCardId: chechCHNCardId,
     arrayMin: arrayMin,
@@ -288,5 +333,8 @@ module.exports = {
     each: each,
     midstr: midstr,
     substartstr: substartstr,
-    subendstr: subendstr
+    subendstr: subendstr,
+    combine: combine, // 生成笛卡尔积
+    requestFullScreen: requestFullScreen, // 进入全屏
+    exitFullscreen: exitFullscreen // 退出全屏
 }
